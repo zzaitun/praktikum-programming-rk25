ebe7b8e5af7791e492303f0bb12ca1576443ad3d
zzaitun <zet.zaitun@gmail.com>
2025-09-27 02:33:14 +0800

    Adding praktikum 1

---
+++ praktikum-1.py
@@ -0,0 +1,91 @@
+print("Praktikum Pengantar Pemrograman")
+print()
+
+print("Nama Lengkap   : Mahasiswa")
+print("NIM            : 6002201014")
+print("Prodi          : Robotika dan AI")
+print()
+
+kampus = "kampus ITH"
+print("Data yang dibuat adalah", kampus)
+print("Tipe datanya adalah", type(kampus))
+print()
+
+x = 15
+print("Data yang dibuat adalah", x)
+print("Tipe datanya adalah", type(x))
+print()
+
+a = 15.5
+print("data adalah", a)
+print("tipe data", type(a))
+print()
+
+z = complex(4,5)
+print("data adalah", z)
+print("tipe data", type(z))
+print()
+
+log = True
+print("data adalah", log)
+print("tipe data", type(log))
+print()
+
+waktu = 1005 #detik
+
+menit = waktu//60
+detik = waktu%60
+
+print("waktu",waktu,"detik sama dengan",menit,"menit dan",detik,"detik")
+print()
+
+# Operator
+
+a = 6
+b = 9
+c = a + b
+print("nilai",a,"+",b,"=",c)
+
+# pengurangan
+# perkalian
+# pembagian
+# pangkat
+# floor
+# modulus
+
+#DIBUAT DALAM FILE BARU (tugas-1.py)
+a = float(input("masukkan nilai a= "))
+b = float(input("masukkan nilai b= "))
+
+#hasil print
+
+#1 nilai a + b = ...
+#2 nilai a - b = ...
+#3 nilai a*b = ...
+#nilai a/b = ...
+#nilai a**2 + b**2 = ...
+#nilai a mod b = 
+#nilai b mod a = 
+#nilai a // b =
+#nilai b // a =
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
