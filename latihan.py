82044ec741b22985dbd4fa7cc134b35e9d08fc18
zzaitun <zet.zaitun@gmail.com>
2025-10-09 15:03:58 +0800

    Tambah file baru

---
+++ latihan.py
@@ -0,0 +1,106 @@
+import os
+os.system('clear')
+
+# print("Hallo Dunia")
+# name = 100
+# Name = 200
+
+# print(name)
+# print(Name)
+
+
+
+# usia = float(input("Input Usia Anda= "))
+
+# print(usia)
+
+# print(type(usia))
+
+
+
+
+
+# x = "halo"
+# y = "saya"
+# z = "adalah user"
+
+# print(x+y+z)
+
+
+
+
+# x = 10
+# y = "mahasiswa"
+
+# print(x,y)
+
+
+
+
+
+
+
+# x1 = "30.0"
+# print("x sebelum diubah tipe datanya adalah",type(x1))
+
+# x2 = float(x1)
+
+# print("x setelah diubah tipe datanya adalah",type(x2))
+
+# x3 = int(x2)
+
+# print("x setelah diubah lagi tipe datanya adalah",type(x3))
+
+# print(x3)
+
+
+# waktu = 130
+
+# detik = waktu%60  #mendapatkan sisa bagi dari 60
+# menit = waktu//60 #mendapatkan hasil bagi dari 60
+
+
+# # Waktu 130 detik sama dengan 2 menit dan 10 detik
+# print("Waktu ",waktu," detik sama dengan ",menit," menit dan ",detik," detik")
+
+
+
+
+# a = 5
+
+# a+=2 #jumlah
+
+# a-=1 #kurang
+
+# a//=4 #floor atau hasil
+
+# print(a)
+
+
+# a = 7
+# b = 3
+
+# print( not(a<9 or b>4) )
+
+# # not(and) ==> NAND
+# # not(or). ==> NOR
+
+
+# kampus = "Institut Teknologi Bacharuddin Jusuf Habibie"
+
+# a = "Habibie"
+
+# print(a not in kampus)
+
+a = 7
+b = 15
+# print(a,'bentuknya= ',format(a,'08b'))
+# print(b,'bentuknya= ',format(b,'08b'))
+
+print('\n ================ >>')
+print('Nilai',a,'dalam biner    = ',format(a,'08b'))
+# print('Nilai',b,'dalam biner    = ',format(b,'08b'))
+print('--------------------------------------->>')
+c = a>>1
+print('Nilai a >>',1,'biner adalah',format(c,'08b'))
+
---
+++ praktikum-2.py
@@ -0,0 +1,8 @@
+import os
+os.system('clear')
+
+print("nama\t\t:Nama Lengkap")
+print("prodi\t\t:Robotika\n")
+
+print("Praktikum 2")
+
